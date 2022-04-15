a6ec9fa41f19b7d7c2e19ff71e65129ff59dcc05
GeovaniPrestes <geovani@sistemasbr.net>
2022-04-15 11:38:34 -0300

    Criado o randomizador de cartas :D

--- js/iniciarSistema.js
+++ js/iniciarSistema.js
@@ -1,4 +1,8 @@
 function IniciarSistema() {
+    AtribuirFuncaoAosBotoes();
+}
+
+function AtribuirFuncaoAosBotoes(){
     document.getElementById("novoJogoF").onclick = function () { IniciarNovoJogo(0) };
     document.getElementById("novoJogoM").onclick = function () { IniciarNovoJogo(1) };
     document.getElementById("novoJogoD").onclick = function () { IniciarNovoJogo(2) };
--- js/script.js
+++ js/script.js
@@ -2,14 +2,36 @@ let vidas = 0;
 let pontos = 0;
 let vidasDificuldade = [10, 5, 3];
 let cartasViradas = 0;
+let posicaoCartas = [-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1];
 
 function IniciarNovoJogo(dificuldade) {
     cartasViradas = 0;
     vidas = vidasDificuldade[parseInt(dificuldade)];
+    SortearPosicaoCartas();
     AtualizarTextos();
 }
 
 function AtualizarTextos() {
     document.getElementById("vidas").innerHTML = "Vidas restantes: " + vidas;
     document.getElementById("pontos").innerHTML = "Pontuação: " + pontos;
+}
+
+function SortearPosicaoCartas() {
+    var posicaoSorteio = [[-1, -1], [-1, -1], [-1, -1], [-1, -1], [-1, -1], [-1, -1], [-1, -1], [-1, -1], [-1, -1]];
+    for (var i = 0; i < 18; i++) {
+        var foto = Math.round(Math.random() * 8);
+        if (posicaoSorteio[foto][0] == -1)
+            posicaoSorteio[foto][0] = i;
+        else if (posicaoSorteio[foto][1] == -1)
+            posicaoSorteio[foto][1] = i;
+        else
+            i--;
+    }
+
+    for(var i = 0; i< 9; i++){
+        posicaoCartas[posicaoSorteio[i][0]] = i;
+        posicaoCartas[posicaoSorteio[i][1]] = i;
+    }
+
+    console.log(posicaoCartas);
 }
