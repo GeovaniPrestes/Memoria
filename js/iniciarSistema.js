4188d125f55b89661cfd0dcf0dfb85e44e19216c
GeovaniPrestes <geovani@sistemasbr.net>
2022-04-15 11:11:13 -0300

    Criando arquivo de regras e adicionando as funcionalidades aos botões

---
+++ js/iniciarSistema.js
@@ -0,0 +1,7 @@
+function IniciarSistema() {
+    document.getElementById("novoJogoF").onclick = function () { IniciarNovoJogo(0) };
+    document.getElementById("novoJogoM").onclick = function () { IniciarNovoJogo(1) };
+    document.getElementById("novoJogoD").onclick = function () { IniciarNovoJogo(2) };
+}
+
+IniciarSistema();
--- js/script.js
+++ js/script.js
@@ -1,8 +1,10 @@
 let vidas = 0;
 let pontos = 0;
 let vidasDificuldade = [10, 5, 3];
+let cartasViradas = 0;
 
 function IniciarNovoJogo(dificuldade) {
+    cartasViradas = 0;
     vidas = vidasDificuldade[parseInt(dificuldade)];
     AtualizarTextos();
 }
@@ -11,4 +13,3 @@ function AtualizarTextos() {
     document.getElementById("vidas").innerHTML = "Vidas restantes: " + vidas;
     document.getElementById("pontos").innerHTML = "Pontuação: " + pontos;
 }
-
